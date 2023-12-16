2e798ce689437801f579d96eb4f846f8dacec46a
Roberta Takenaka <roberta.takenaka@scielo.org>
2023-12-16 10:27:05 -0300

    adiciona comando para registrar o resultado de pip freeze (PROGDIR/logs)

--- src/scielo/bin/xml/install_requirements.bat
+++ src/scielo/bin/xml/install_requirements.bat
@@ -1,1 +1,7 @@
-python -m pip install -U -r app_modules/settings/requirements.txt
+
+cd %1
+dir
+if not exist %2 (
+    python -m pip freeze >> %2
+)
+python -m pip install -U -r .\app_modules\settings\requirements.txt
